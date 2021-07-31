b9c2b2c2bf04caf1a6ab2412d1ecbc33c0cdbbcf
regojoyson <rego.joyson@gmail.com>
2021-07-31 13:40:25 +0530

    updated date programmes

--- src/test/java/org/example/DateJava7.java
+++ src/test/java/org/example/DateJava7.java
@@ -8,14 +8,14 @@ import java.util.GregorianCalendar;
 
 public class DateJava7 {
 
-    private static void printDate(String date, int type) throws ParseException {
+    private static String getDate(String date, int N) throws ParseException {
         Date parsedDate = null;
         Calendar calenderDate = new GregorianCalendar();
         calenderDate.setLenient(false);
         SimpleDateFormat d = null;
 
         try {
-            switch (type) {
+            switch (N) {
                 case 1:
                     d = new SimpleDateFormat("dd/MM/yyyy");
                     d.setLenient(false);
@@ -33,23 +33,23 @@ public class DateJava7 {
                     break;
             }
             calenderDate.setTime(parsedDate);
-            System.out.println(calenderDate.get(Calendar.DAY_OF_MONTH) + " " + (calenderDate.get(Calendar.MONTH) + 1) + " " + calenderDate.get(Calendar.YEAR));
+            return calenderDate.get(Calendar.DAY_OF_MONTH) + " " + (calenderDate.get(Calendar.MONTH) + 1) + " " + calenderDate.get(Calendar.YEAR);
 
         } catch (ParseException ex) {
-            System.out.println("-1 -1 -1");
+            return "-1 -1 -1";
 
         }
-        System.out.println("-------------------------7");
+
     }
 
     public static void main(String[] args) throws ParseException {
         //     DateTimeFormatter f = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         //    System.out.println(LocalDate.parse("11/10/2020",f));
-        printDate("11/10/2020", 1);
-        printDate("2020.11.10", 2);
-        printDate("Oct 11, 2020", 3);
-        printDate("32/10/2020", 1);
-        printDate("11/01/2020", 1);
+        System.out.println(getDate("11/10/2020", 1));
+        System.out.println(getDate("2020.11.10", 2));
+        System.out.println(getDate("Oct 11, 2020", 3));
+        System.out.println(getDate("32/10/2020", 1));
+        System.out.println(getDate("11/01/2020", 1));
 
 
     }
--- src/test/java/org/example/LocalDate8Qqtn.java
+++ src/test/java/org/example/LocalDate8Qqtn.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
@@ -9,12 +10,11 @@ public class LocalDate8Qqtn {
     private static DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy.MM.dd");
     private static DateTimeFormatter formatter3 = DateTimeFormatter.ofPattern("MMM dd, yyyy");
 
-    private static void printDate(String date, int type) {
+    private static String getDate(String date, int N) {
         LocalDate parsedDate = null;
         try {
 
-
-            switch (type) {
+            switch (N) {
                 case 1:
                     parsedDate = LocalDate.parse(date, formatter1);
                     break;
@@ -25,23 +25,24 @@ public class LocalDate8Qqtn {
                     parsedDate = LocalDate.parse(date, formatter3);
 
             }
-            System.out.println(parsedDate.getDayOfMonth() + " " + parsedDate.getMonthValue() + " " + parsedDate.getYear());
+            return parsedDate.getDayOfMonth() + " " + parsedDate.getMonthValue() + " " + parsedDate.getYear();
         } catch (DateTimeParseException ex) {
-            System.out.println("-1 -1 -1");
+            return "-1 -1 -1";
 
         }
-        System.out.println("-------------------------");
+
 
     }
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws ParseException {
         //     DateTimeFormatter f = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         //    System.out.println(LocalDate.parse("11/10/2020",f));
-        printDate("11/10/2020", 1);
-        printDate("2020.11.10", 2);
-        printDate("Oct 11, 2020", 3);
-        printDate("32/10/2020", 1);
-        printDate("11/01/2020", 1);
+        System.out.println(getDate("11/10/2020", 1));
+        System.out.println(getDate("2020.11.10", 2));
+        System.out.println(getDate("Oct 11, 2020", 3));
+        System.out.println(getDate("32/10/2020", 1));
+        System.out.println(getDate("11/01/2020", 1));
 
 
     }
